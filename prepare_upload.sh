f01d8ff25745074a826fc652271b19983fd3ff5d
Wildapfel <maxpetzold@yahoo.de>
2026-09-16 12:31:31 +0200

    first commit

---
+++ prepare_upload.sh
@@ -0,0 +1,11 @@
+DEST="/var/home/max-inspiron/GitHub/Wildapfel/portfolio/projects/pangenome/homo-sapiens-chr21/"
+SRC="."
+
+rsync -av \
+    $DEST \
+    $SRC
+
+for f in data/*/*.fna ; do
+    head $f > $f.snippet;
+    rm $f
+done    
